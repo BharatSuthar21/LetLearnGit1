8e77b74e2d0a84edd779f2d25ac182abb835eadd
Bharat Suthar <bharat21suthar@gmail.com>
2022-11-12 18:50:30 +0530

    Initial Commit

---
+++ code1.py
@@ -0,0 +1,9 @@
+def boys_score(score1,score2):
+	return score1+score2
+score3=boys_score(23,45)
+def girls_score(score1,score2):
+	return score1+score2
+score4=girls_score(89,98)
+def avg_score():
+	return (score3+score4)/4
+print(avg_score())
